c7bdc07c24d380fd6e3dd2c1bb69cd856cfee376
Santiago Villa <code@santiagovilla.it>
2025-02-06 21:46:10 +0100

    formatting changes

--- src/question.js
+++ src/question.js
@@ -1,32 +1,14 @@
 class Question {
-   constructor (text, choices, answer, difficulty) {
-     this.text = text;
-     this.choices = choices;
-     this.answer = answer;
-     this.difficulty = difficulty;
-
-   }
-   shuffleChoices() {
-        for (let i = this.choices.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
     }
-    }
+  }
 }
-
-
-
-
-
-
-
-// // Get the shuffled array from the function
-// const newResult = shuffleChoices(choices); 
-
-// // Display Shuffled array
-// console.log("Shuffled Array: ", newResult);
-
-
-
-
-
--- src/quiz.js
+++ src/quiz.js
@@ -1,54 +1,30 @@
 class Quiz {
-
-    constructor (questions, timeLimit, timeRemaining,) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.currentQuestionIndex = 0;
-        this.correctAnswers = 0;
-    }
-getQuestion() {
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.currentQuestionIndex = 0;
+    this.correctAnswers = 0;
+  }
+  getQuestion() {
     return this.questions[this.currentQuestionIndex];
-}
+  }
 
-    moveToNextQuestion() {
-        this.currentQuestionIndex++;
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [this.questions[j],this.questions[i]];
     }
-
-
-
-
-shuffleQuestions() {
-    for(let i = this.questions.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+  }
+  checkAnswer(answer) {
+    if (answer === true) {
+      this.correctAnswers++;
     }
-}
-checkAnswer(answer) { 
-    if (answer === true ){ 
-        this.correctAnswers++;}
-    
+  }
 
-}
-
-hasEnded() {
-
-
-}
-
-
-
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  hasEnded() {}
 }
