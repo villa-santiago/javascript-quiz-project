015f2e3d66a8da9f1e5756e2cb7a8ee7a193a834
Yuji Uono <yuuono.1998@gmail.com>
2025-02-06 21:18:23 +0100

    day1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,32 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+   constructor (text, choices, answer, difficulty) {
+     this.text = text;
+     this.choices = choices;
+     this.answer = answer;
+     this.difficulty = difficulty;
 
-    // 2. shuffleChoices()
+   }
+   shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+    }
+    }
 }
+
+
+
+
+
+
+
+// // Get the shuffled array from the function
+// const newResult = shuffleChoices(choices); 
+
+// // Display Shuffled array
+// console.log("Shuffled Array: ", newResult);
+
+
+
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,43 @@
 class Quiz {
+
+    constructor (questions, timeLimit, timeRemaining,) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.currentQuestionIndex = 0;
+        this.correctAnswers = 0;
+    }
+getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+}
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+
+
+
+
+shuffleQuestions() {
+    for(let i = this.questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+    }
+}
+checkAnswer(answer) { 
+    if (answer === true ){ 
+        this.correctAnswers++;}
+    
+
+}
+
+hasEnded() {
+
+
+}
+
+
+
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
