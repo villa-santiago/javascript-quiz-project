1072e5d46893dc80659ef8b3bf90fdd587384146
Yuji Uono <yuuono.1998@gmail.com>
2025-02-08 16:20:21 +0100

    day2complete

--- src/quiz.js
+++ src/quiz.js
@@ -37,25 +37,38 @@ checkAnswer(answer) {
     }
     
 
-
-
-
-
 hasEnded() {
-
-
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+if (this.currentQuestionIndex < this.questions.length){
+    return false;
+} else if(this.currentQuestionIndex === this.questions.length) {
+    return true;
 }
+}
+
+filterQuestionsByDifficulty(difficulty) {
+if (typeof difficulty !== "number" || difficulty < 1 || difficulty > 3){
+    return;
+}
+this.questions = this.questions.filter(questions => questions.difficulty === difficulty)
+}
+
+averageDifficulty() {
+   const totalDifficulty = this.questions.reduce((accumulator, question)=>{
+    return accumulator + question.difficulty;
+   }, 0);
+
+   const averageLevel = totalDifficulty / this.questions.length;
+   return averageLevel;    
+}
+
+
+
+// You should use the reduce() method to sum the difficulty of all the questions and then divide the sum by the number of questions to get the average difficulty.
+
+
+// averageDifficulty() method
+
+
+// should return the average difficulty of the questions in the quiz
+
 }
